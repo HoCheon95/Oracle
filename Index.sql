762029cb3856a369c826e62f9408bae4ed00c1b0
HoCheon95 <dcr584@gmail.com>
2025-12-26 17:42:30 +0900

    update

--- Index.sql
+++ Index.sql
@@ -10,3 +10,46 @@
     삽입, 수정, 삭제의 과정을 거칠 때마다 요구되는 사항이다.
   · 데이터를 수정하는데 있어서 더 많은 시간이 소요된다.
   · 따라서 인덱스는 꼭 필요한 곳에만 적절하게 사용
+ - 인덱스의 분류
+  · Unique / Non-Unique
+  · Single / Composite
+  · 자동 / 수동 Index
+  · Normal Index( B-tree Index ) / Bitmap Index / Function-based Normal Index
+사용형식)
+  CREATE [UNIQUE|BITMAP] INDEX 인덱스명 ON 테이블명 (컬럼1[, 컬럼2, ...][ASC|DESC]
+
+사용예) CART 테이블과 REMAIN 테이블을 Cartesian Product 하여 
+       테이블 tb1_idx로 저장하시오
+  CREATE TABLE tbl_idx(R_YEAR, R_PID, RJ_00, R_I, R_O, RJ_99, R_DATE,
+                       C_MID, C_NO, C_PID, C_QTY) AS
+    SELECT * FROM REMAIN CROSS JOIN CART;
+  COMMIT;
+  SELECT COUNT(*) FROM TBL_IDX;
+
+사용예) 테이블 TBL_IDX에서 R_PID가 'P201000005'이고 C_NO가 '2020041000001'인 
+       자료를 조회하시오
+  SELECT * 
+    FROM TBL_IDX
+   WHERE R_PID = 'P201000005' 
+     AND C_NO = '2020041000001';
+
+  인덱스 생성)
+  CREATE INDEX idx_temp ON TBL_IDX(R_PID, C_NO);
+ 
+ 
+사용예) CART 테이블에서 장바구니번호와 상품코드를 결합한 후 13번째부터 5글자로 인덱스를 구성하시오.
+  CREATE INDEX idx_cart_no ON CART(SUBSTR(CART_NO||PROD_ID,13,5));
+  
+사용에) CART_NO와 PROD_ID의 결합 데이터 중 '2P302' 자료를 검색하시오
+  SELECT * FROM CART
+   WHERE SUBSTR(CART_NO||PROD_ID,13,5)='2P302';
+
+  DROP INDEX idx_cart_no;
+  COMMIT;
+
+** INDEX 의 재구성
+  - 대상 테이블의 데이터 변경이 많이 발생된 경우
+  - 테이블의 저장 위치가 바뀐 경우(TABLESPACE 가 바뀐 경우)
+ 사용형식)
+  ALTER INDE 인덱스명 REBUILD;
+ 
